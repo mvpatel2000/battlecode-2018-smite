f664f0ef43fd40963020ccd779b291f79872a17f
justinz2018 <justinz2018@users.noreply.github.com>
2018-01-28 19:41:29 -0500

    Update Factory.java

--- finalplayer-java/Factory.java
+++ finalplayer-java/Factory.java
@@ -60,7 +60,7 @@ public class Factory {
             Globals.gc.produceRobot(unit.id(), UnitType.Worker);
         else if(Globals.current_round>550 && Globals.num_workers<4 && Globals.gc.canProduceRobot(unit.id(), UnitType.Worker))
             Globals.gc.produceRobot(unit.id(), UnitType.Worker);
-        else if(Globals.num_mages<10 && Globals.num_rangers>Globals.num_mages*1.5 && num_enemy_rangers < 5 && Globals.num_healers>Globals.num_mages*1.25)
+        else if(Globals.num_mages<8 && Globals.num_rangers>Globals.num_mages*1.5 && num_enemy_rangers < 4 && Globals.num_healers>Globals.num_mages*1.25)
             Globals.gc.produceRobot(unit.id(), UnitType.Mage);
         else if(Globals.num_rangers<2)
             Globals.gc.produceRobot(unit.id(), UnitType.Ranger);
