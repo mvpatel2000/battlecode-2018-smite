444ed9783e1957ccd65b5bea56e332a9ddccf6c5
Justin Z <justinrz2018@gmail.com>
2018-01-30 16:05:32 +0000

    added files

---
+++ finalplayer-java/RocketLaunch.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import bc.*;
+
+public class RocketLaunch 
+{
+	int land_round;
+	MapLocation loc;
+	public RocketLaunch(int land_round, MapLocation loc) 
+	{
+		this.land_round = land_round;
+		this.loc = loc;
+	}
+}
