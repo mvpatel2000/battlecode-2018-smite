a8c20fe0cb636a1e386a1f97d92e02c1dafb847a
Mihir Patel <mihir.v.patel7@gmail.com>
2018-01-30 17:45:34 -0500

    major rocket bug

--- finalplayer-java/Rocket.java
+++ finalplayer-java/Rocket.java
@@ -174,6 +174,7 @@ public class Rocket {
             if(x==enem_loc[0] && y==enem_loc[1]) {
                 Globals.enemy_locations.remove(i);
                 Globals.rocket_homing--;
+                PathShits.buildFieldBFS();
                 return;
             }
         }
